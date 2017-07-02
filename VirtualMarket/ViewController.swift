b47fb635f98fbdeec03ea60125dc14bb80b0ddb5
nishaanthkanna <nishaanth1998@gmail.com>
2017-07-02 22:47:41 +0530

    First Commit

---
+++ VirtualMarket/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VirtualMarket
+//
+//  Created by Nishaanth Kanna Ravichandran on 02/07/17.
+//  Copyright © 2017 Nishaanth Kanna Ravichandran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
