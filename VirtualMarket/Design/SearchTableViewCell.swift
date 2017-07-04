d28bcb29b22c7f93c54c63e6678ed92e67893676
nishaanthkanna <nishaanth1998@gmail.com>
2017-07-05 00:38:42 +0530

    Added search function

---
+++ VirtualMarket/Design/SearchTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SearchTableViewCell.swift
+//  VirtualMarket
+//
+//  Created by Nishaanth Kanna Ravichandran on 04/07/17.
+//  Copyright © 2017 Nishaanth Kanna Ravichandran. All rights reserved.
+//
+
+import UIKit
+
+class SearchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
