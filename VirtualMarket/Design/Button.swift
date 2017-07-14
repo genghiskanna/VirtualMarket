d616b5f2621778ae5a497aeeede806710006c344
nishaanthkanna <nishaanth1998@gmail.com>
2017-07-14 23:56:40 +0530

    designed 90%

---
+++ VirtualMarket/Design/Button.swift
@@ -0,0 +1,35 @@
+//
+//  Button.swift
+//  VirtualMarket
+//
+//  Created by Nishaanth Kanna Ravichandran on 14/07/17.
+//  Copyright © 2017 Nishaanth Kanna Ravichandran. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class Button: UIButton{
+    
+    @IBInspectable var text: String?
+    
+    override init(frame: CGRect) {
+        super.init(frame:frame)
+        setupButton()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupButton()
+    }
+    
+    func setupButton(){
+        backgroundColor = Colors.teal
+        layer.borderColor = Colors.teal.cgColor
+        layer.borderWidth = 1.0
+        layer.cornerRadius = 5.0
+        
+        titleLabel?.text = self.text
+        titleLabel?.textColor = Colors.light
+        
+    }
+}
